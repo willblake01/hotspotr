c9e0d411da6b0cde46b3a0c87de841af23e12ba4
William Blake <willblakebooking@gmail.com>
2026-06-13 15:01:13 -0500

    HOT-26: Convert search query to geographic coordinates

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { profile } from './profile';
+import locationReducer from '../store/locationSlice';
 
 export const rootReducer = combineReducers({
   user: profile,
+  location: locationReducer,
 });
